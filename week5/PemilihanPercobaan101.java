488e6049354f246a91f38fb902be00e3d6437497
Agilabrs <arbiansyah.agil11@gmail.com>
2023-09-25 12:11:03 +0700

    after

--- week5/PemilihanPercobaan101.java
+++ week5/PemilihanPercobaan101.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class PemilihanPercobaan101 {
     public static void main(String[] args) {
         Scanner input00 = new Scanner(System.in);
+        int angka;
         System.out.print("Masukkan angka: ");
-        int angka = input00.nextInt();
-        
-        if (angka %2 == 0) 
-            System.out.println("Angka "+angka+" Bilangan Genap");
-        else 
-            System.out.println("Angka "+angka+" Bilangan Ganjil");
+         angka = input00.nextInt();
+    
+        System.out.println("Angka "+angka+" bilangan "+(angka%2 == 0 ? "genap" : "ganjil"));
+    
 }
     
 }
