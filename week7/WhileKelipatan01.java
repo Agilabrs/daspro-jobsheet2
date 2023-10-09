a360455d652f7845ba105d4af14523f4f77312ae
Agilabrs <arbiansyah.agil11@gmail.com>
2023-10-09 14:15:04 +0700

    task 4

---
+++ week7/WhileKelipatan01.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class WhileKelipatan01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int kelipatan, rata;
+        int jumlah = 0, counter = 0, i = 1;
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        while (i <= 50) {
+            if (i % kelipatan == 0){
+            jumlah += i;
+            counter++;
+            }
+            i++;
+        }   rata = (jumlah / counter);
+            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+            System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+            System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rata);
+    }
+    
+}
