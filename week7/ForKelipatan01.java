0d68cf61b7ca4c6d2477caf340ae3c2bb4c3ecac
Agilabrs <arbiansyah.agil11@gmail.com>
2023-10-09 12:30:11 +0700

    modif

--- week7/ForKelipatan01.java
+++ week7/ForKelipatan01.java
@@ -4,19 +4,21 @@ public class ForKelipatan01 {
     public static void main(String[] args) {
         Scanner scan01 = new Scanner(System.in);
 
-        int kelipatan;
+        int kelipatan, rata;
         int jumlah = 0, counter = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = scan01.nextInt();
 
-        for (int i = 1; i <= 50; i++ ){
+        for (int i = 1; i <= 50; i++){
             if (i % kelipatan == 0)
             jumlah += i;
             counter++;
+            
         }
-        System.out.printf("Banyaknya bilangan &d dari 1 sampai 50 adalah %d\n",kelipatan, counter);
-        System.out.printf("Banyaknya bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-
+        rata = (jumlah / counter);
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rata);
     }
     
 }
