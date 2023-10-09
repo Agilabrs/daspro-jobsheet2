b759de610ee8743d4aa578f83c0aae90bc171761
Agilabrs <arbiansyah.agil11@gmail.com>
2023-10-09 14:12:34 +0700

    modif

--- week7/ForKelipatan01.java
+++ week7/ForKelipatan01.java
@@ -10,10 +10,10 @@ public class ForKelipatan01 {
         kelipatan = scan01.nextInt();
 
         for (int i = 1; i <= 50; i++){
-            if (i % kelipatan == 0)
+            if (i % kelipatan == 0){
             jumlah += i;
             counter++;
-            
+            }
         }
         rata = (jumlah / counter);
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
