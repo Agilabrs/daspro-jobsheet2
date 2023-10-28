40319c93807236985acdef6c6e2e3ee7cf0f9a6f
Agilabrs <arbiansyah.agil11@gmail.com>
2023-10-28 20:11:25 +0700

    modifikasi

--- week9/ArrayRataNilai01.java
+++ week9/ArrayRataNilai01.java
@@ -3,20 +3,30 @@ public class ArrayRataNilai01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // input variabel
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        int[] nilaiMhs = new int[5];
+        double totalLulus = 0, totalTdkLulus = 0;
+        double rata2lulus = 0, rata2tdklulus = 0;
         // looping
         for (int i=0; i<nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = input.nextInt();
         }
+        // modif
+       
+         int tdkLulus = 0, lulus = 0;
         for (int i=0; i<nilaiMhs.length; i++){
-            total += nilaiMhs[i];
+            if (nilaiMhs[i]>70) {
+                totalLulus += nilaiMhs[i];
+                lulus++;
+            }else{
+                totalTdkLulus += nilaiMhs[i];
+                tdkLulus++;
+            }
         }
         // rumus rata rata
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai "+rata2);
-
+        rata2lulus = totalLulus/lulus;
+        rata2tdklulus = totalTdkLulus/tdkLulus;
+        System.out.println("Rata-rata nilai yang lulus: "+rata2lulus);
+        System.out.println("Rata-rata nilai yang tidak lulus:"+rata2tdklulus);
     }
 }
