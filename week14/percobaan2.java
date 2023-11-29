4c3793230a8e350cbaaac19b4d9a85a030df229d
Agilabrs <arbiansyah.agil11@gmail.com>
2023-11-29 15:08:56 +0700

    modified'

--- week14/percobaan2.java
+++ week14/percobaan2.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 public class percobaan2 {
     static int hitungPangkat(int x, int y){
         if (y == 0){
+            System.out.println("*0 = 1");
             return(1);
         }else{
-            return(x *hitungPangkat(x, y-1));
+            int hasilRekursif = (x *hitungPangkat(x, y-1));
+            System.out.println(x+" * "+y+" = "+hasilRekursif);
+            for (int i =0;i<=y;i++) {
+                System.out.print(x + " * ");
+            }
+            System.out.print(hasilRekursif);
+            System.out.println();
+            return hasilRekursif;
         }
     }
     public static void main(String[] args) {
